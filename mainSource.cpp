47f9edd009be3fcc43ac9d21610bcf9aa638feed
Aldrick T <taldrick@gmail.com>
2023-05-19 00:29:34 -0600

    Attempt 1 helloworld

---
+++ mainSource.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	cout << "Type your name: ";
+	cin >> name;
+	cout << "Age: ";
+	cin >> age;
+	cout << "Hello " << name << ". You are" << age << "years old." endl;
+	return 0;
+}
